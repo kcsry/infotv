2e0c47e959126bfa9b6f6165b25f824c18d79f9c
Aarni Koskela <akx@iki.fi>
2017-08-02 14:37:06 +0300

    Add exponential backoff to infotv_cache_weather

--- infotv/management/commands/infotv_cache_weather.py
+++ infotv/management/commands/infotv_cache_weather.py
@@ -1,5 +1,6 @@
 # -- encoding: UTF-8 --
 import requests
+import time
 from django.core.management.base import BaseCommand
 from infotv.models import Datum
 
@@ -12,6 +13,19 @@ class Command(BaseCommand):
     def handle(self, location_id, app_id, **options):
         if not app_id:
             raise ValueError("app_id required")
+
+        for attempt in range(1, 11):
+            try:
+                self.retrieve_weather(location_id, app_id)
+                return
+            except requests.HTTPError as he:
+                if he.response.status_code == 401:  # Reraise unauthorized immediately
+                    raise
+                self.stderr.write("Attempt %d failed (%s), trying again soon..." % (attempt, he))
+                time.sleep(1 ** attempt)
+        raise RuntimeError("All attempts at retrieving data failed.")
+
+    def retrieve_weather(self, location_id, app_id):
         resp = requests.get(
             url="http://api.openweathermap.org/data/2.5/weather",
             params={"id": location_id, "appid": app_id}
