9dabf24fb0e7375788ce9ceccf2e8c9b338f58b9
Tomi Simsiö <logima@iki.fi>
2018-06-06 00:34:27 +0300

    Anime-slide with image & text

--- infotv/frontend/src/s/anime-slide.jsx
+++ infotv/frontend/src/s/anime-slide.jsx
@@ -2,13 +2,28 @@ import React from "react";
 import DatumManager from "../datum";
 
 function AnimeSlide() {
-    const anime = DatumManager.getValue("anime", {now: "", next: ""});
+    const anime = DatumManager.getValue("anime", {now: {name: "", image: "", text: ""}, next: {name: "", image: "", text: ""}});
     return (
         <div className="slide anime-slide">
-            <div className="now-header">Animehuoneessa nyt:</div>
-            <div className="now">{anime.now}</div>
-            <div className="next-header">Seuraavaksi:</div>
-            <div className="next">{anime.next}</div>
+            <div className="slide-header">Animehuone</div>
+            <div className="container">
+                {anime.now.name &&
+                <div className="now">
+                    <div className="header">Nyt menossa</div>
+                    <div className="name">{anime.now.name}</div>
+                    <img src={anime.now.image} alt=""/>
+                    <div className="text">{anime.now.text}</div>
+                </div>
+                }
+                {anime.next.name &&
+                <div className="next">
+                    <div className="header">Seuraavaksi vuorossa</div>
+                    <div className="name">{anime.next.name}</div>
+                    <img src={anime.next.image} alt=""/>
+                    <div className="text">{anime.next.text}</div>
+                </div>
+                }
+            </div>
         </div>
     );
 }
