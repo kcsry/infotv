708ff28bfe6257a085993917e9e430991f5aeaa2
Aarni Koskela <akx@iki.fi>
2018-06-07 16:53:43 +0300

    Prepare Webpack and so on for Typescriptation

--- infotv/frontend/webpack.config.js
+++ infotv/frontend/webpack.config.js
@@ -1,51 +1,63 @@
-const webpack = require("webpack");
-const autoprefixer = require("autoprefixer");
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
 
-const CURRENT_STYLE = process.env.INFOTV_STYLE || "desucon";
+const CURRENT_STYLE = process.env.INFOTV_STYLE || 'desucon';
 const outputFsPath = process.env.OUTPUT_PATH || `${__dirname}/../static/infotv`;
-const outputPublicPath = process.env.PUBLIC_PATH || "/static/infotv";
-const production = process.argv.indexOf("-p") !== -1;
+const outputPublicPath = process.env.PUBLIC_PATH || '/static/infotv';
 
-const config = {
+module.exports = (env, argv) => ({
     context: __dirname,
-    entry: ["whatwg-fetch", "./src/main.jsx"],
+    entry: ['whatwg-fetch', './src/main.tsx'],
     bail: true,
-    devtool: "source-map",
+    devtool: 'source-map',
     output: {
         path: outputFsPath,
-        filename: "bundle.js",
+        filename: 'bundle.js',
         publicPath: outputPublicPath,
     },
     module: {
-        loaders: [
+        rules: [
             {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: "babel",
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'awesome-typescript-loader',
+                    },
+                ],
             },
             {
-                test: /\.(woff|woff2|svg|otf|ttf|eot|png|json)(\?.*)?$/,
-                loader: "url",
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.(woff|woff2|svg|otf|ttf|eot|png)(\?.*)?$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                    },
+                ],
             },
         ],
     },
     resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+        symlinks: false,  // SPEED BOOST
         alias: {
-            "current-style": `../styles/${CURRENT_STYLE}/less/style.less`,
+            'current-style': `../styles/${CURRENT_STYLE}/less/style.less`,
         },
     },
-    plugins: [new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en|fi/)],
-    postcss: [autoprefixer({ browsers: ["last 2 versions"] })],
-};
-
-if (production) {
-    config.plugins.push(
+    plugins: [
+        new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en|fi/),
         new webpack.DefinePlugin({
-            "process.env": {
-                NODE_ENV: JSON.stringify("production"),
+            'process.env': {
+                NODE_ENV: JSON.stringify(argv.mode === 'production' ? 'production' : 'development'),
             },
-        })
-    );
-}
-
-module.exports = config;
+        }),
+    ],
+});
