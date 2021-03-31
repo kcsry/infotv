8336d04bbb784759fe2688cc2af728117fb2310d
Aarni Koskela <akx@iki.fi>
2021-03-31 12:13:53 +0300

    Become Python 3.6+ only

--- setup.py
+++ setup.py
@@ -10,13 +10,18 @@ setup(
     author="Aarni Koskela",
     author_email="akx@iki.fi",
     url="https://github.com/kcsry/infotv",
-    packages=find_packages('.', exclude=('infotv_test*',)),
+    packages=find_packages(".", exclude=("infotv_test*",)),
     package_data={"infotv": ["static/infotv/*"]},
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'Django>=1.8',
-        'jsonfield>=1.0.3',
-        'requests>=2.13.0',
+        "Django>=1.8",
+        "jsonfield>=1.0.3",
+        "requests>=2.13.0",
     ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
 )
