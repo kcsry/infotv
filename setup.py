0601c933ccb1114abd47757c89267d7a74a7934a
Aarni Koskela <akx@iki.fi>
2021-03-31 12:39:43 +0300

    Drop support for Django < 2.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        "Django>=1.8",
+        "Django>=2.2",
         "jsonfield>=1.0.3",
         "requests>=2.13.0",
     ],
