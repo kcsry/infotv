178eae0e6c510d68ed2730d69a30aa432d2f3d13
Aarni Koskela <akx@iki.fi>
2021-06-21 20:23:48 +0300

    Become 0.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="infotv",
-    version="0.3.0",
+    version="0.4.0",
     description="A television, with information",
     author="Aarni Koskela",
     author_email="akx@iki.fi",
